fa037232cda1c7506f41696cb62a70f53e74f34c
Olivier <olivier.huguenot@gmail.com>
2025-04-08 09:45:25 +0200

    fix version ignore

--- include/version.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#define GIT_HASH        F("f5aec32")
-#define GIT_BRANCH      F("main")
-#define GIT_TAG         F("0.0.0")
-#define GIT_TAG_MAJOR   (0)
-#define GIT_TAG_MINOR   (0)
-#define GIT_TAG_PATCH   (0)
